1607932071ba3ca8b673dbf6eeb9935b17210337
Cyri0 <you@example.com>
2025-03-31 13:52:53 +0200

    base done

--- game.js
+++ game.js
@@ -1,22 +1,49 @@
 const framePerRow = 3;
 const framePerColumn = 4;
-const imgWidth = 144;
-const imgHeight = 256;
-
-const frameWidth = imgWidth / framePerRow;
-const frameHeight = imgHeight / framePerColumn;
 const imgSrc = "sprite.png"
+
+let direction = "KeyS"
 
 function initImage(){
     const viewPortEl = document.createElement("div")
     viewPortEl.className = "viewPort"
-
+    viewPortEl.style.aspectRatio = `${framePerRow} / ${framePerColumn}`
     const imgEl = document.createElement("img")
     imgEl.src = imgSrc
-
-    
+    imgEl.style.width = (framePerRow * 100) + '%'
 
     viewPortEl.appendChild(imgEl)
     document.body.appendChild(viewPortEl)
+    
+    doSomeAnimation(imgEl)
 }
+
 initImage()
+
+function doSomeAnimation(img){
+    let offset = 0;
+    setInterval(()=>{
+        img.style.left = offset + "%"
+        offset -= 100
+        img.style.top = calculateVertical()
+        if(offset <= (framePerRow *100)*-1)
+            offset = 0
+    }, 100)
+}
+
+function calculateVertical(){
+    switch (direction) {
+        case "KeyW": return "0%"; break;
+        case "KeyA": return "-300%"; break;  
+        case "KeyS": return "-200%"; break;
+        case "KeyD": return "-100%"; break;
+    }
+}
+
+document.addEventListener("keydown", (k)=>{
+    direction = k.code
+})
+
+document.addEventListener("keyup", (k)=>{
+    direction = "KeyS"
+})
